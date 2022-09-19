64c31d2d44c086bef5122bbca1357af1b998987e
Awfulot <Mr.Snisken1@gmail.com>
2022-09-19 14:35:15 -0400

    Added DaisyUI and test button

--- src/IftttAd.js
+++ src/IftttAd.js
@@ -11,6 +11,7 @@ const IftttAd = () => {
                 <h1>More, faster, better Applets</h1>
                 <p>Get access to 20 Applets, multiple actions, and other advanced features.</p>
                 <button>Try it free</button>
+                <button className="btn btn-active btn-primary">DaisyUI Test</button>
 
             </div>
         </div>
