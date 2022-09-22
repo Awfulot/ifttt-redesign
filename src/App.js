4d86573ce511e68b79d5321a6567b9b72759987a
Snisken <Mr.Snisken1@gmail.com>
2022-09-22 10:57:51 -0400

    recommit via ubuntu terminal

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import TopNav from "./TopNav";
 import Content from "./Content";
 import Footer from "./Footer";
 import IftttAd from "./IftttAd";
-//import React from "@types/react";
+//yimport React from "@types/react";
 
 function App() {
   return (
