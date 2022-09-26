5377fa50d25857a98fbbca0604badffe02b8d46b
Snisken <Mr.Snisken1@gmail.com>
2022-09-26 14:42:55 -0400

    Made the content nav in the search section component functional

--- src/Content.js
+++ src/Content.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SearchSection from "./SearchSection";
 //import DisplayGridCard from "./DisplayGridCard";
 import AllCardData from "./AllCardData";
@@ -8,27 +8,13 @@ import AppletsCardData from "./AppletsCardData";
 
 const Content = () => {
 
-    const displayAllCards = true;
-    const displayStoryCards =  false;
-    const displayServicesCard = false;
-    const displayAppletsCard = false;
+
 
     return (
         <div className="w-1/2 mx-auto">
             <h1 className="text-3xl font-bold text-center mt-6">Explore</h1>
             <SearchSection/>
-            {
-                displayAllCards && <AllCardData/>
-            }
-            {
-                displayStoryCards && <StoriesCardData/>
-            }
-            {
-                displayServicesCard && <ServicesCardData/>
-            }
-            {
-                displayAppletsCard && <AppletsCardData/>
-            }
+
 
 
 
--- src/SearchSection.js
+++ src/SearchSection.js
@@ -1,14 +1,77 @@
 import React from 'react';
+import {useState} from "react";
+import AllCardData from "./AllCardData";
+import StoriesCardData from "./StoriesCardData";
+import ServicesCardData from "./ServicesCardData";
+import AppletsCardData from "./AppletsCardData";
 
 const SearchSection = () => {
+    const [displayAllCards, setDisplayAllCards] = useState(true);
+    const [displayStoryCards, setDisplayStoryCards] =  useState(false);
+    const [displayServicesCard, setDisplayServicesCard] = useState(false);
+    const [displayAppletsCard, setDisplayAppletsCard] = useState(false);
+
+    const [allCardTabStatus, setAllCardTabStatus] = useState("tab-active")
+    const [storyCardTabStatus, setStoryCardTabStatus] = useState("")
+    const [servicesCardTabStatus, setServicesCardTabStatus] = useState("")
+    const [appletsCardTabStatus, setAppletsCardTabStatus] = useState("")
+
+    const allCardsClick = () => {
+        setDisplayAllCards(true)
+        setDisplayStoryCards(false)
+        setDisplayServicesCard(false)
+        setDisplayAppletsCard(false)
+
+        setAppletsCardTabStatus("")
+        setAllCardTabStatus("tab-active")
+        setStoryCardTabStatus("")
+        setServicesCardTabStatus("")
+    };
+    const storyCardsClick = () => {
+        setDisplayAllCards(false)
+        setDisplayStoryCards(true)
+        setDisplayServicesCard(false)
+        setDisplayAppletsCard(false)
+
+        setAppletsCardTabStatus("")
+        setAllCardTabStatus("")
+        setStoryCardTabStatus("tab-active")
+        setServicesCardTabStatus("")
+    };
+    const servicesCardsClick = () => {
+        setDisplayAllCards(false)
+        setDisplayStoryCards(false)
+        setDisplayServicesCard(true)
+        setDisplayAppletsCard(false)
+
+        setAppletsCardTabStatus("")
+        setAllCardTabStatus("")
+        setStoryCardTabStatus("")
+        setServicesCardTabStatus("tab-active")
+    };
+    const appletsCardsClick = () => {
+        setDisplayAllCards(false)
+        setDisplayStoryCards(false)
+        setDisplayServicesCard(false)
+        setDisplayAppletsCard(true)
+
+        setAppletsCardTabStatus("tab-active")
+        setAllCardTabStatus("")
+        setStoryCardTabStatus("")
+        setServicesCardTabStatus("")
+    };
+
+
+
+
     return (
         <div>
             <div>
                 <div className="tabs flex space-x-10 justify-evenly">
-                    <a className="tab tab-bordered">All</a>
-                    <a className="tab tab-bordered tab-active">Applets</a>
-                    <a className="tab tab-bordered">Services</a>
-                    <a className="tab tab-bordered">Stories</a>
+                    <a onClick={() => allCardsClick()} className={`tab tab-bordered ${allCardTabStatus}`}>All</a>
+                    <a onClick={() => appletsCardsClick()} className={`tab tab-bordered ${appletsCardTabStatus}`}>Applets</a>
+                    <a onClick={() => servicesCardsClick()} className={`tab tab-bordered ${servicesCardTabStatus}`}>Services</a>
+                    <a onClick={() => storyCardsClick()} className={`tab tab-bordered ${storyCardTabStatus}`}>Stories</a>
                 </div>
             </div>
             <div className="form-control">
@@ -20,7 +83,18 @@ const SearchSection = () => {
 
                 </div>
             </div>
-
+            {
+                displayAllCards && <AllCardData/>
+            }
+            {
+                displayStoryCards && <StoriesCardData/>
+            }
+            {
+                displayServicesCard && <ServicesCardData/>
+            }
+            {
+                displayAppletsCard && <AppletsCardData/>
+            }
         </div>
     );
 };
